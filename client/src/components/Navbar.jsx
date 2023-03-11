b3186a9697f04afc8b2fbe91314797d753ac4682
Wassim Mansouri <wassim.mansouri@gmail.com>
2023-03-11 09:29:12 +0100

    Install react-icons, ethers

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
+import { HiMenut4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import logo from "../../images/logo.png";
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4"></nav>
+  );
 };
 
 export default Navbar;
